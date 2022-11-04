b3212aace1f53af6329952429c5318d86ece9086
Eray <86014603+eraybtgn@users.noreply.github.com>
2022-11-04 17:25:03 +0300

    Update Daire.java

--- src/Daire.java
+++ src/Daire.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Daire {
+import java.util.Scanner;
+
+public class Daire {
+    public static void main(String[] args) {
+        double yariCap;
+        double pi=3.14;
+        System.out.println("Yarı çapı giriniz..");
+        Scanner scan= new Scanner(System.in);
+        yariCap=scan.nextInt();
+        double alan=pi*(Math.pow(yariCap,2));
+        double cevre= 2*pi*yariCap;
+        System.out.println("Dairenin alanı:"+alan+" ve çevresi:"+cevre);
+    }
 }
